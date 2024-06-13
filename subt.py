df047bcc1bd9b7a08714d6982dc77a597ea0d24d
Daniel Weber <prof.daniel.weber@gmail.com>
2024-06-13 09:11:02 -0300

    adicao da linha

--- subt.py
+++ subt.py
@@ -9,32 +9,39 @@ color = '#b4963c'
 
 
 image_arq = 'Capa aula 1200x768.png'
-text = 'O Início da Carreira Psicanalítica de Freud #1'.upper()
-left_pos = 120
-right_pos = 200
+text = 'O Início da Carreira '.upper()
+left_pos = 120  # PIXEL
+vert_pos  = 75  # PERCENTUAL %
+right_pos = 200 # PIXEL
 font_size = 60
 
 img = Image.open(dir_origem + image_arq)
-# print(img.size)
+print('IMAGE SIZE: ', img.size)
 font = ImageFont.truetype(font_path, font_size)
 draw = ImageDraw.Draw(img)
-image_widht, image_height = img.size
+image_width, image_height = img.size
 left, top, right, bottom = bbox = font.getbbox(text)
 # bbox = font.getbbox(text)
-print(bbox)
-text_widht = right - left
+print("text_box(bbox): ", bbox)
+text_width = right - left
 text_height = bottom - top
+print("text_width: ", text_width)
 print("text_height: ", text_height)
-
+top_pos = round(image_height*vert_pos/100) - bottom 
+print("top_pos: ", top_pos)
 draw.text(
-    #((image_widht -text_widht) / 2, (image_height - text_height) / 2),
-    (left_pos,(3*image_height/4)-bottom),
+    #((image_width -text_width) / 2, (image_height - text_height) / 2),
+    (left_pos,top_pos),
     # (100,500),
     text,
     fill=color,
-    font=font
+    font=font,
+    # stroke_width=2,
+    # stroke_fill='black'
 )
-
+line_height = top_pos+text_height+30
+line_width = text_width + left_pos + 100
+draw.line((0,line_height,line_width,line_height), fill=color, width=5)
 
 # Verifique se o pastaetório já existe
 if not os.path.exists(dir_destino):
